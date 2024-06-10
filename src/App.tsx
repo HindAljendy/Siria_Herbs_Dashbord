f61d6e2c1daa478575ba90ccefb927d37e059a53
HindAljendy <isonhindjendy1998@gmail.com>
2024-06-10 04:18:01 +0300

    Neven Section Navbar,ProductCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
+import Navbar from './componnents/Navbar/Navbar'
+import ProductBox from './componnents/ProductBox/ProductBox'
+import img from './assets/images/Product/product_01.png'
 
 function App() {
-  
+
   return (
     <>
-      <h1>Siria Herbs</h1>
+    {/* neven Section : */}
+      <Navbar />
+      <ProductBox img={img} title_1="للتنحيف و انقاص الوزن"
+        title_2="شاي اخضر سوري مع التوت البري" 
+        title_3="خلطة طبيعية و مدعمة للتنحيف و انقاص الوزن" 
+        brandName="a vie" productNum={8} weight="55G" encapsolation="معبأة في كيس نايلون وعلبة كرتون خاصة" 
+        numInPackage={10} 
+      />
+      
     </>
   )
 }
 
 export default App
+
+
---
+++ src/types/types.tsx
@@ -0,0 +1,12 @@
+export interface ProductBoxProps {
+    img: string;
+    title_1: string;
+    title_2: string;
+    title_3: string;
+    brandName: string;
+    productNum: number;
+    weight: string;
+    encapsolation: string;
+    numInPackage: number;
+}
+
