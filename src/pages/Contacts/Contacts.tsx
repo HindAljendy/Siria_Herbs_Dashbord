cd011f3a78b78d971086482e2d4d70c635631721
Abdllauhramdan <abdram2003@gmail.com>
2024-06-20 22:47:43 +0300

    add th big navigation and link between front and back

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import AddContact from '../../componnents/Form/AddContact/AddContact' ;
+import NavigationLinks from '../../componnents/NavigationLinks/NavigationLinks';
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+    <div>
+        <NavigationLinks 
+        navigateMain="اعدادات جهات الاتصال " 
+        navigateLink="لوحة التحكم " 
+        navigateSubmain="الاعدادات" 
+      />
+    <AddContact contactId="1"/>
+    </div>
   )
 }
 
