2acc9020532a9518d2e7cb47b9ae752dc1adbcf5
HindAljendy <142920133+HindAljendy@users.noreply.github.com>
2024-06-20 10:24:53 +0300

    Update Navigation Links width

--- src/componnents/Form/TextArea/TextArea.tsx
+++ src/componnents/Form/TextArea/TextArea.tsx
@@ -9,7 +9,7 @@ const TextArea: React.FC<TextArea> = ({ name }) => {
   return (
     <div className='input'>
       <label htmlFor="description"  className="HJ_FontColor_gray"> {name}</label>
-      <textarea name="description" id="description" className='MA_TextArea' ></textarea>
+      <textarea name="description" id="description" className='MA_TextArea'></textarea>
     {/*  <div className='MA_Note'>سيظهر هذا النص فوق الصورة</div>*/}
     </div>
   )
