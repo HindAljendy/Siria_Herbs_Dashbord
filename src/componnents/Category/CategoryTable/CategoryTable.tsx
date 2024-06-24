16eb34cbbb2e9a01d7d7bb072c194716b787b527
YousefSaleh1 <148710592+YousefSaleh1@users.noreply.github.com>
2024-06-24 22:06:06 +0300

    Update

--- src/componnents/Category/CategoryTable/CategoryTable.tsx
+++ src/componnents/Category/CategoryTable/CategoryTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect ,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './CategoryTable.css';
 import delet from "../../../assets/images/button_icon/delete.svg";
 import edite from "../../../assets/images/button_icon/edite.svg";
@@ -11,48 +11,49 @@ import { useNavigate } from 'react-router-dom';
 import Delete_CategoryPopup from '../Delete_CategoryPopup/Delete_CategoryPopup';
 
 type TDataitem = {
-    id?: string;
+    id?: number;
     name?: string;
     published?: boolean;
-    
+    brands_name: string[];
+    products_count: number;
 }
 type TData = Array<TDataitem>;
 
 
 const CategoryTable: React.FC<TableProps> = ({ title, buttonLabel, columns }) => {
-    const[data,setData] = useState<TData>([]);
+    const [data, setData] = useState<TData>([]);
     const [showPopup, setShowPopup] = useState(false);
-    const [itemId, setItemId] = useState<string | null>(null);
+    const [itemId, setItemId] = useState<number | undefined>();
 
     const navigate = useNavigate();
 
     const handleClick = () => {
-      navigate('/categories/addCategory');
+        navigate('/categories/addCategory');
     };
 
-   
-    useEffect(()=> {
-        axios.get("http://127.0.0.1:8000/api/categorys").then((res: any)=>setData(res.data)
-        ).catch((error:any)=>console.log(error))
-    });
-  
-    const deleteItem = (itemId: string) => {
-        setItemId(itemId); 
-        setShowPopup(true); 
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/categorys").then((res) => setData(res.data.data)
+        ).catch((error) => console.log(error))
+    },[]);
+
+    const deleteItem = (itemId: number | undefined) => {
+        setItemId(itemId);
+        setShowPopup(true);
     };
-    
-  
-    const handleEdit = (itemId: string) => {
+
+
+    const handleEdit = (itemId: number | undefined) => {
         navigate(`/categories/update-category/${itemId}`);
     };
 
-    
+
     return (
         <div className="table-container">
             <div className="table-header">
                 <h2>{title}</h2>
-                <button  className='NA_button_Name' onClick={handleClick}>
-                  <img src={plus} alt = "" style={{margin:'10px'}}/>   {buttonLabel}
+                <button className='NA_button_Name' onClick={handleClick}>
+                    <img src={plus} alt="" style={{ margin: '10px' }} />   {buttonLabel}
                 </button>
 
             </div>
@@ -66,37 +67,37 @@ const CategoryTable: React.FC<TableProps> = ({ title, buttonLabel, columns }) =>
                     </tr>
                 </thead>
                 <tbody >
-                    {data.map((row:any) => {
+                    {data?.map((row: TDataitem) => {
                         return (
                             <tr>
-                            <td className='NA_Name_row'>{row.name}</td>
-                            <td>
-                            <ul style={{listStyle:'none'}}>
-                                {row.brands.map((brand: any) => (
-                                <li>{brand.name}</li>
-                                ))}
-                            </ul>
-                            </td>
-                            <td> {row.products_count}  </td>
-                            <td>
-                                {row.published ? <img src={switch_on} alt="switch-on " className='NA_product-image'/> : <img src={switch_off} alt="switch_off " className='NA_product-image'/>}
-                            </td>
-                            <td>
-                                <div className="ne-action NA_display">
-                                   
-                                <div className="ne-delete-icon" style={{ cursor: 'pointer' }}>
-                                    <img src={delet} alt="delete icon" className='delete-edite' onClick={() => deleteItem(row.id)} />
-                                    {showPopup && (
-                                    <Delete_CategoryPopup id={itemId || ''} onClose={() => setShowPopup(false)} />
-                                   )}
+                                <td className='NA_Name_row'>{row.name}</td>
+                                <td>
+                                    <ul style={{ listStyle: 'none' }}>
+                                        {row.brands_name.map((brand: string) => (
+                                            <li>{brand}</li>
+                                        ))}
+                                    </ul>
+                                </td>
+                                <td> {row.products_count} </td>
+                                <td>
+                                    {row.published ? <img src={switch_on} alt="switch-on " className='NA_product-image' /> : <img src={switch_off} alt="switch_off " className='NA_product-image' />}
+                                </td>
+                                <td>
+                                    <div className="ne-action NA_display">
 
-                                </div>
-                                    <div className="ne-edit-icon " style={{cursor:'pointer'}}>
-                                        <img src={edite} alt="edite icon"  className='delete-edite' onClick={() => handleEdit(row.id)} />
+                                        <div className="ne-delete-icon" style={{ cursor: 'pointer' }}>
+                                            <img src={delet} alt="delete icon" className='delete-edite' onClick={() => deleteItem(row.id)} />
+                                            {showPopup && (
+                                                <Delete_CategoryPopup id={itemId} onClose={() => setShowPopup(false)} />
+                                            )}
+
+                                        </div>
+                                        <div className="ne-edit-icon " style={{ cursor: 'pointer' }}>
+                                            <img src={edite} alt="edite icon" className='delete-edite' onClick={() => handleEdit(row.id)} />
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                        </tr>
+                                </td>
+                            </tr>
                         );
                     })}
                 </tbody>
--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NavigationLinks from '../../componnents/NavigationLinks/NavigationLinks'
-import ProductsOperationsSection from '../../componnents/ProductsOperationsSection/ProductsOperationsSection'
+// import ProductsOperationsSection from '../../componnents/ProductsOperationsSection/ProductsOperationsSection'
 // import Table from '../../componnents/Table/Table'
 import CategoryTable from '../../componnents/Category/CategoryTable/CategoryTable'
 
@@ -12,7 +12,7 @@ const Category = () => {
           navigateMain='أضف فئة'
           navigateLink='الواجهة الرئيسية' navigateSubmain='الفئات' 
         />
-      <ProductsOperationsSection />
+      {/* <ProductsOperationsSection /> */}
       <Outlet/>
       <CategoryTable title={'الفئات'} buttonLabel={'اضف فئة'} columns={['الاسم', 'العلامات التجارية', 'عدد المنتجات', 'تم النشر', 'الإجراءات']} data={[]}/>
     </>
