2e65cba021b1c8a5bfa9b5fb33470cc11372914e
rashait95 <rashabaakhras@gmial.com>
2024-06-25 11:20:48 +0300

    add Navigation Link

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { StoryForm } from '../../componnents/StoryForm/StoryForm'
 
 const Main = () => {
   return (
     <div>
       <h1>Main</h1>
+      <StoryForm/>
     </div>
   )
 }
