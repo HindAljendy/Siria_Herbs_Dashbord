563a4ac96737ff460c670ea20fc6f87203419368
YousefSaleh1 <148710592+YousefSaleh1@users.noreply.github.com>
2024-06-26 12:41:36 +0300

    Update Main

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import SettingsAbout from '../../componnents/SettingAbout/SettingsAbout'
 // import Certifica from '../../componnents/Form/CertificaForm/Certifica'
 // import CertificaTable from '../../componnents/Table/CertificaTable'
 
 const Main = () => {
   return (
     <div>
-      <h1>Main</h1>
-      <SettingsAbout/>
-      {/* <Certifica/>
-      <CertificaTable title="الشهادات" buttonLabel="إضافة شهادة"  columns={["الايقونة" , "الاسم" ,"الاسم الفرعي" ,"الصورة" , "الوصف","الإجرائات"]}  
+      main
+      {/* <Certifica/> */}
+      {/* <CertificaTable title="الشهادات" buttonLabel="إضافة شهادة"  columns={["الايقونة" , "الاسم" ,"الاسم الفرعي" ,"الصورة" , "الوصف","الإجرائات"]}  
       data={
         [
           {
